7d7533ac8ca2fefe4bfee2aeb39d9821c446aa22
Den <den>
2020-10-14 17:39:43 +0700

    day update

--- Source/UnrealOptimizer/Private/OptimizerActorComponent.cpp
+++ Source/UnrealOptimizer/Private/OptimizerActorComponent.cpp
@@ -3,14 +3,29 @@
 
 #include "OptimizerActorComponent.h"
 
+#include "Components/PrimitiveComponent.h"
+#include "Runtime/Renderer/Public/PrimitiveSceneInfo.h"
+
 // Sets default values for this component's properties
 UOptimizerActorComponent::UOptimizerActorComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+#if WITH_EDITORONLY_DATA 	// ...Usefull Flag Optimizer 
+	ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	if (ArrowComponent)
+	{
+		ArrowComponent->SetHiddenInGame(false);
+		ArrowComponent->ArrowColor = FColor(255, 0, 0);
+		ArrowComponent->bTreatAsASprite = true;
+		ArrowComponent->bIsScreenSizeScaled = true;
+		ArrowComponent->SetRelativeLocation(FVector(0,0,120));
+		ArrowComponent->SetRelativeScale3D(FVector(2, 2, 2));
+	}
+#endif // WITH_EDITORONLY_DATA
 }
 
 
@@ -18,7 +33,7 @@ UOptimizerActorComponent::UOptimizerActorComponent()
 void UOptimizerActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
+	UE_LOG(LogTemp, Log, TEXT("Optimizer Activated!"));
 	// ...
 	
 }
@@ -29,6 +44,40 @@ void UOptimizerActorComponent::TickComponent(float DeltaTime, ELevelTick TickTyp
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	bRenderNow = WasRecentlyRendered(RenderDelay);
+
+	//ArrowComponent->SetVisibility(!bRenderNow);
 	// ...
+}
+
+//
+bool UOptimizerActorComponent::CheckRenderInfo(float &lastTime)
+{
+	if (const UWorld* const World = GetWorld())
+	{
+		AActor* Owner = GetOwner();
+		UPrimitiveComponent* PC = Cast<UPrimitiveComponent>(Owner->GetRootComponent());
+		FPrimitiveSceneProxy* SP = PC->SceneProxy;
+		FPrimitiveSceneInfo* PSI = SP->GetPrimitiveSceneInfo();
+		lastTime = PC->GetLastRenderTime();
+		return true;
+	};
+		//GetLastRenderTimeOnScreen(); -1 =/
+	//lastTime = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent())->SceneProxy->GetPrimitiveSceneInfo()->LastRenderTime;
+	return false;
+}
+
+bool UOptimizerActorComponent::WasRecentlyRendered(float &DelayRender, float Tolerance /*= 0.2*/) const
+{
+	if (const UWorld* const World = GetWorld())
+	{
+		// Adjust tolerance, so visibility is not affected by bad frame rate / hitches.
+		const float RenderTimeThreshold = FMath::Max(Tolerance, World->DeltaTimeSeconds + KINDA_SMALL_NUMBER);
+
+		// If the current cached value is less than the tolerance then we don't need to go look at the components
+		DelayRender = World->TimeSince(GetOwner()->GetLastRenderTime());
+		return DelayRender <= RenderTimeThreshold; //World->TimeSince(GetOwner()->GetLastRenderTime()) <= RenderTimeThreshold;
+	}
+	return false;
 }
 
--- Source/UnrealOptimizer/Public/OptimizerActorComponent.h
+++ Source/UnrealOptimizer/Public/OptimizerActorComponent.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+
+#include "Components/ArrowComponent.h"
+
 #include "OptimizerActorComponent.generated.h"
 
 
@@ -12,9 +15,23 @@ class UNREALOPTIMIZER_API UOptimizerActorComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	/** Component shown in the editor only to indicate optimizer component*/
+#if WITH_EDITORONLY_DATA 
+	UPROPERTY()
+	UArrowComponent* ArrowComponent;
+#endif 
 public:	
 	// Sets default values for this component's properties
 	UOptimizerActorComponent();
+	
+	/** Main tick function for the Component */
+    // r.VisualizeOccludedPrimitives 1
+
+	//Render Property Info
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UnrealOptimizer")
+	bool bRenderNow;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UnrealOptimizer")
+	float RenderDelay = 0.0;
 
 protected:
 	// Called when the game starts
@@ -24,5 +41,16 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable, Category = "UnrealOptimizer", meta = (WorldContext = WorldContextObject))
+	bool CheckRenderInfo(float &lastTime);
+
+	/**
+	 * My Custom. Returns true if this component has been rendered "recently", with a tolerance in seconds to define what "recent" means.
+	 * e.g.: If a tolerance of 0.1 is used, this function will return true only if the actor was rendered in the last 0.1 seconds of game time.
+	 *
+	 * @param Tolerance  How many seconds ago the actor last render time can be and still count as having been "recently" rendered.
+	 * @return Whether this actor was recently rendered.
+	 */
+	UFUNCTION(Category = "UnrealOptimizer", BlueprintCallable, meta = (DisplayName = "WasComponentRecentlyRenderedOptimizer", Keywords = "scene visible"))
+	bool WasRecentlyRendered(float &DelayRender,float Tolerance = 0.2) const;
 };
